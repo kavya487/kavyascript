fb0722be1d5f335982c146751cb0992a60dbf248
kavya478 <kummithakavya@gmail.com>
2024-12-30 15:36:42 +0530

    read the username

---
+++ 3-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=$1
+Person2=$2
+
+echo "$Person1:: Hello $Person2"
+echo "$Person2:: Hi $Person1"
+echo "$Person1:: what are you doing?"
+echo "$Person2:: I am eating lunch"
---
+++ 4-variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "enter the field"
+
+read "field"
