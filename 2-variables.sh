c0694a82b407e194234844cc6399a3b2729b67d6
kavya478 <kummithakavya@gmail.com>
2024-12-30 15:12:55 +0530

    added text

---
+++ 2-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=Kavya
+Person2=Divya
+
+echo  "$person1:: Hello $Person2"
+echo "$Person2:: Hi $Person1"
+echo "$Person1:: what are you doing?"
+echo "$Person2:: I am eating lunch"
