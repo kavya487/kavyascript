301f8ae45a7d4a6efb6d446006e195948115bbd8
kavya478 <kummithakavya@gmail.com>
2025-01-01 21:41:28 +0530

    numbers in 100

--- 7-conditions.sh
+++ 7-conditions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-num=$1
+num=$a
 if [ $num -gt 100]
 then
        echo "number is greater than 100" 
 else 
-        echo "given num is less than 100"
+        echo "given num is less  than 100"
 fi
