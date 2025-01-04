0d93981ec4a295272d016d813403eed390af8bad
kavya478 <kummithakavya@gmail.com>
2025-01-04 12:10:22 +0530

    hyd res

--- 8-mysql.sh
+++ 8-mysql.sh
@@ -9,4 +9,19 @@ if [ $userid -ne 0 ]
 fi
 
  dnf install mysql -y
+if  [ $? -ne 0 ]
+ then 
+      echo "Error: "installing mysql... failure"
+      exit 1
+else
+      echo "Error: "Installing mysql ... success"
+fi        
+ 
  dnf install git -y
+ if  [ $? -ne 0 ]
+ then 
+      echo "Error: "installing git... failure"
+      exit 1
+else
+      echo "Error: "Installing git ... success"
+fi   
