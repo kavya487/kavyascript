ff5d87d7a7fae9d2ceb03243f4a4b0575d169d7e
kavya478 <kummithakavya@gmail.com>
2025-01-24 21:06:44 +0530

    add script

--- arrays.sh
+++ arrays.sh
@@ -1,9 +1,9 @@
  Movies=("Pushpa" "RRR" "GC")
- echo list of array is ${Movies{0}}
+ echo list of array is ${Movies[0]}
 
    echo list of array is ${Movies[1]}
    
    echo list of array is ${Movies[2]}
    
-   echo list of array is ${Movies[3]}
+   echo full list arrays ${Movies[0,1,2]}
 
