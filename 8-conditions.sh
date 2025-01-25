c3a3707ed8d49598695779fce17375025a59c92a
kavya478 <kummithakavya@gmail.com>
2025-01-25 07:42:14 +0530

    add

---
+++ 8-conditions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+NUM=$!
+if [ NUM -gt 100 ] 
+then 
+echo "given num is greater than 100"
+else 
+echo "given num is less than 100"
+
--- conditions.sh
+++ conditions.sh
@@ -5,5 +5,5 @@ if (Day==$sunday)
 then
 echo Print today is holiday
 else 
-echo today is not holiday
+echo today is not holiday 
 fi
