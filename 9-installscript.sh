17d69ad0a1c0e154cc014a7f86202c88a09e30a4
kavya478 <kummithakavya@gmail.com>
2025-01-25 08:12:12 +0530

    add

--- 9-installscript.sh
+++ 9-installscript.sh
@@ -3,7 +3,9 @@
  userid=$(id -u)
 if [ $userid -ne 0 ]
 then
-echo "User is having access to install script"
+echo "User is  not having access to install script"
 fi
 
 dnf install mysql -y
+
+dnf install git -y
