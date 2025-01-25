7378df660d7e5677fa687a482a6f2d2af5fa7b56
kavya478 <kummithakavya@gmail.com>
2025-01-25 08:29:44 +0530

    add

--- 9-installscript.sh
+++ 9-installscript.sh
@@ -1,13 +1,21 @@
 #!?bin/bash
 
  userid=$(id -u)
+
 if [ $userid -ne 0 ]
-then
-echo "User is  not having access to install script"
-exit 0
-fi
+    then
+    echo "User is  not having access to install script"
+    exit 1
+    fi
 
 dnf install mysqll -y
+then 
+   echo "installing myscript"
+   fi
 
 
 dnf install git -y
+then 
+   echo "installing git"
+   fi
+
