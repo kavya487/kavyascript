470eec18c42df0c3ec628014e4c8c55074794e65
kavya478 <kummithakavya@gmail.com>
2025-09-25 00:36:39 +0530

    add

--- 14-loops.sh
+++ 14-loops.sh
@@ -2,7 +2,32 @@
 
 uid=$(id -u)
 
-log_folder="/var/log/shellscript-logs/
-log_file=$((echo $0) | cut -d "." -f1)
-timestamp=$(date +%Y-%m-%D-%H-%M-%S)
+log_folder="/var/log/shellscript-logs/"
+log_file=$(echo $0 | cut -d "." -f1)
+timestamp=$(date +%Y-%m-%d-%H-%M-%S)
+log_file_name="log_folder/log-file-timestamp.log"
 
+if [ $uid -ne 0]
+then
+    echo "u must have root access"
+    exit 1
+fi
+
+echo "script was executed at  $log_file_name
+
+for package in $@
+do
+
+dnf list installed $package >>&$log_file_name
+
+then
+dnf install $package -y
+        if [ $? -ne 0 ]
+        then 
+        echo "$package was not installed" >>$log_file_name
+        else
+        echo "$package was installed successfully" >>$log_file_name
+else
+echo "$package was already installed"
+fi
+done
