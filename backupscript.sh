5b9b162f7bdc14bc425850485dfeeb1af625e34e
kavya478 <kummithakavya@gmail.com>
2025-09-26 10:36:07 +0530

    add

--- backupscript.sh
+++ backupscript.sh
@@ -7,7 +7,7 @@
 
 source_dir=$1
 dest_dir=$2
-days=${3: -14}
+days=${3:-14}
 
 if [ $# -lt 2 ]
 then
@@ -21,19 +21,19 @@ timestamp=$(date +%Y-%m-%d)
 log_file_name="$log_folder/$log_file-$timestamp.log"
 
 mkdir -p /home/ec2-user/shellscript-logs
-if [  ! -d  $source_dir ]
+if [  ! -d  "$source_dir" ]
 then
     echo "please provide valid source path"
     exit 1
 fi
 
-if [ ! -d  $dest_dir ]
+if [ ! -d  "$dest_dir" ]
 then
     echo "please provide valid dest path"
     exit 1
 fi
 
-files=$(find $source_dir -name "*.log" -mtime $days)
+files=$(find $source_dir -name "*.log" -mtime +$days)
 
 if [ -n $files ]
 then
