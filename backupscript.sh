91ea6075ad9990c78030855d9d41a36def2f555c
kavya478 <kummithakavya@gmail.com>
2025-09-26 10:12:16 +0530

    add

---
+++ backupscript.sh
@@ -0,0 +1,35 @@
+archive the logs 
+
+create soirce dir 
+dest dir 
+days
+
+
+source_dir=$1
+dest_dir=$2
+days=${3: -14}
+
+if [ $# -lt 2 ]
+then
+    echo "please provide sh script-name <source-dir> <dest-dir> <optional><days>"
+        exit 1
+fi
+
+log_folder="/home/ec2-user/shellscript-logs"
+log_file=$(echo $0 | cut -d "." -f1)
+timestamp=$(date +%Y-%m-%d)
+log_file_name="$log_folder/$log_file-$timestamp.log"
+
+mkdir -p /home/ec2-user/shellscript-logs
+if [ -d ! $source_dir ]
+then
+    echo "please provide valid source path"
+    exit 1
+fi
+
+if [ -d ! $dest_dir ]
+then
+    echo "please provide valid dest path"
+    exit 1
+fi
+
